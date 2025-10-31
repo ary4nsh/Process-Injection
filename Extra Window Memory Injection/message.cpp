d952f240593b924a4e0f4c982782d92b634955c0
ary4nsh <aryansh.github@gmail.com>
2025-10-31 07:44:03 -0800

    Update message.cpp

--- Extra Window Memory Injection/message.cpp
+++ Extra Window Memory Injection/message.cpp
@@ -1,11 +1,13 @@
 #include <windows.h>
 
 int main() {
+    // Display a message box in the current process indicating the injection was successful
     MessageBoxW(
-        NULL,                        // No owner window
-        L"Injected!",                // Message box content
-        L"ListPlanting",             // Message box title
-        MB_OK | MB_ICONINFORMATION   // Button and icon type
-    );
+        NULL,                            // hWnd: No owner window specified
+        L"Injected!",                    // lpText: Content of the message box
+        L"ListPlanting",                 // lpCaption: Title of the message box
+        MB_OK | MB_ICONINFORMATION       // uType: Button and icon type (OK button with information icon)
+    );  
     return 0;
+
 }
